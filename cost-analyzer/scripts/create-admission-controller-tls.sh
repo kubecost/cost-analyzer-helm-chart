cc56f30ecb9cc19fb8ac975f32397c32ead7307b
Thomas Nguyen <thomasn@kubecost.com>
2023-10-09 18:13:29 -0700

    Adds documentation for the Kubecost Admission Controller (#2629)
    * Adds documentation for the Kubecost Admission Controller
    
    * Updates after running shellcheck linter

--- cost-analyzer/scripts/create-admission-controller-tls.sh
+++ cost-analyzer/scripts/create-admission-controller-tls.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
 namespace=$1
-if [ "$namespace" == "" ]; then
+if [[ "${namespace}" == "" ]]; then
   namespace=kubecost
 fi
 
-DIRECTORY=$(cd `dirname $0` && pwd)
-
-echo "Creating certificates"
+echo -e "\nCreating certificates ..."
 mkdir certs
 openssl genrsa -out certs/tls.key 2048
-openssl req -new -key certs/tls.key -out certs/tls.csr -subj "/CN=webhook-server.$namespace.svc"
-openssl x509 -req -days 500 -extfile <(printf "subjectAltName=DNS:webhook-server.$namespace.svc") -in certs/tls.csr -signkey certs/tls.key -out certs/tls.crt
+openssl req -new -key certs/tls.key -out certs/tls.csr -subj "/CN=webhook-server.${namespace}.svc"
+openssl x509 -req -days 500 -extfile <(printf "subjectAltName=DNS:webhook-server.%s.svc" "${namespace}") -in certs/tls.csr -signkey certs/tls.key -out certs/tls.crt
 
-echo "Creating Webhook Server TLS Secret"
+echo -e "\nCreating Webhook Server TLS Secret ..."
 kubectl create secret tls webhook-server-tls \
     --cert "certs/tls.crt" \
-    --key "certs/tls.key" -n $namespace
+    --key "certs/tls.key" -n "${namespace}"
 
-
-echo "Updating values.yaml"
-ENCODED_CA=$(cat certs/tls.crt | base64 | tr -d '\n')
-sed -i 's@${CA_BUNDLE}@'"$ENCODED_CA"'@g' ../values.yaml
+echo -e "\nUpdating values.yaml ..."
+ENCODED_CA=$(base64 < certs/tls.crt | tr -d '\n')
+sed -i '' 's@${CA_BUNDLE}@'"${ENCODED_CA}"'@g' ../values.yaml
